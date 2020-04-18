cfd8e6e1a6a3c8ce14ba0f7be1a06095ece57207
César Díaz <cesar.dl88@gmail.com>
2020-04-18 14:26:44 -0500

    Adding bootstrap

--- administrator/bienvenido.php
+++ administrator/bienvenido.php
@@ -37,85 +37,100 @@ if (!isset($_POST['user'])) {
 if ($band == 1) {
     ?>
     <!DOCTYPE html>
-    <html>
-        <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <link type="text/css" href="style/style.css" rel="stylesheet" media="screen"/>
-            <script type="text/javascript" src="scritps/jquery.js"></script>
-            <script type="text/javascript">
-                $(document).ready(function(){
-                    $('.a_delete').click(function(){
-                        if(confirm('Seguro que desea eliminar este vehiculo?')) {
-                            var href = $(this).attr('href');
-                            var id_product = href.substr(1);
-                            $.post("VehiculoDelete.php",{
-                                id_producto: id_product
-                            },function(data){
-                                $("#result").html(data);
-                                $("#fila-"+id_product).remove();
-                            })
-                        }
-                    });
-                                                                                                                                                                                                
-                    $('#result').click(function() {
-                        $(this).hide();
-                    });
-                });
-            </script>
-            <title>Bienvenido</title>
-        </head>
-        <body>
-            <table style="text-align: right; width: 100%;">
-                <tr>
-                    <td colspan="2" style="font-weight: bold;">
-                        <a href="index.php?param=1">Cerrar sesi&oacute;n</a>
-                    </td>
-                </tr>
-                <tr><td colspan="2"><hr /></td></tr>
-                <tr>
-                    <td style="font-weight: bold; font-size: 14px; text-align: left;">
-                        Mantenimiento de veh&iacute;culos
-                    </td>
-                    <td><a href="formProducto.php?action=N">Nuevo producto</a></td>
-                </tr>
-            </table>
-            <div id="result"></div>
-            <table border="1">
-                <thead>
-                    <tr>
-                        <th>Clase</th>
-                        <th>Modelo</th>
-                        <th>Descripci&oacute;n</th>
-                        <th colspan="4">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
+    <html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="shortcut icon" type="image/jpg" href="favicon.ico"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+              crossorigin="anonymous">
+        <title>Bienvenido</title>
+    </head>
+    <body>
+    <div class="container pt-4">
+        <table style="text-align: right; width: 100%;">
+            <tr>
+                <td colspan="2" style="font-weight: bold;">
+                    <a href="index.php?param=1">Cerrar sesi&oacute;n</a>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <hr/>
+                </td>
+            </tr>
+            <tr>
+                <td style="font-weight: bold; font-size: 14px; text-align: left;">
+                    <h3>Mantenimiento de veh&iacute;culos</h3>
+                </td>
+                <td><a href="formProducto.php?action=N">Nuevo producto</a></td>
+            </tr>
+        </table>
+        <div id="result"></div>
+        <table class="table table-striped">
+            <thead class="thead-dark">
+            <tr>
+                <th>Clase</th>
+                <th>Modelo</th>
+                <th>Descripci&oacute;n</th>
+                <th colspan="4" style="text-align: center;">Acciones</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            include_once 'bussinessLogic/ProductoBL.php';
+
+            $productoBL = new ProductoBL();
+
+            $productos = $productoBL->getProductos();
+            for ($i = 0; $i < count($productos); $i++) {
+                $master = $productos[$i]->getMaster();
+                ?>
+                <tr id="fila-<?php echo $productos[$i]->getId_producto() ?>">
                     <?php
-                    include_once 'bussinessLogic/ProductoBL.php';
+                    echo "<td>{$master->getClase()}</td>";
+                    echo "<td>{$productos[$i]->getModelo()}</td>";
+                    echo '<td>' . $productos[$i]->getDescripcion() . '</td>';
+                    echo "<td><a href='formProducto.php?accion=E&id_producto={$productos[$i]->getId_producto()}&id_master={$master->getId_master()}&mod_prod={$productos[$i]->getModelo()}'>Editar</a></td>";
+                    echo "<td><a href='formDetalles.php?id_producto={$productos[$i]->getId_producto()}&mod_prod={$productos[$i]->getModelo()}'>Detalles</a></td>";
+                    echo "<td><a href='formColores.php?id_producto={$productos[$i]->getId_producto()}&mod_prod={$productos[$i]->getModelo()}'>Colores</a></td>";
+                    echo "<td><a href='#{$productos[$i]->getId_producto()}' class='a_delete'>Eliminar</a></td>";
+                    ?>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.a_delete').click(function () {
+                if (confirm('Seguro que desea eliminar este vehiculo?')) {
+                    var href = $(this).attr('href');
+                    var id_product = href.substr(1);
+                    $.post("VehiculoDelete.php", {
+                        id_producto: id_product
+                    }, function (data) {
+                        $("#result").html(data);
+                        $("#fila-" + id_product).remove();
+                    })
+                }
+            });
 
-                    $productoBL = new ProductoBL();
-
-                    $productos = $productoBL->getProductos();
-                    for ($i = 0; $i < count($productos); $i++) {
-                        $master = $productos[$i]->getMaster();
-                        ?>
-                        <tr id="fila-<?php echo $productos[$i]->getId_producto() ?>" 
-                            onmouseover="this.style.backgroundColor='#E0E0E0'" 
-                            onmouseout="this.style.backgroundColor='#FFFFFF'">
-                                <?php
-                                echo "<td>{$master->getClase()}</td>";
-                                echo "<td>{$productos[$i]->getModelo()}</td>";
-                                echo '<td class="tdDescripcion">' . $productos[$i]->getDescripcion() . '</td>';
-                                echo "<td class='tdAccion'><a href='formProducto.php?accion=E&id_producto={$productos[$i]->getId_producto()}&id_master={$master->getId_master()}&mod_prod={$productos[$i]->getModelo()}'>Editar</a></td>";
-                                echo "<td class='tdAccion'><a href='formDetalles.php?id_producto={$productos[$i]->getId_producto()}&mod_prod={$productos[$i]->getModelo()}'>Detalles</a></td>";
-                                echo "<td class='tdAccion'><a href='formColores.php?id_producto={$productos[$i]->getId_producto()}&mod_prod={$productos[$i]->getModelo()}'>Colores</a></td>";
-                                echo "<td class='tdAccion'><a href='#{$productos[$i]->getId_producto()}' class='a_delete'>Eliminar</a></td>";
-                                ?>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </body>
+            $('#result').click(function () {
+                $(this).hide();
+            });
+        });
+    </script>
+    </body>
     </html>
 <?php }
 ?>
--- administrator/index.php
+++ administrator/index.php
@@ -4,51 +4,63 @@ To change this template, choose Tools | Templates
 and open the template in the editor.
 -->
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link type="text/css" href="style/style.css" rel="stylesheet" media="screen"/>
-        <title>Motos Buffalo</title>
-    </head>
-    <body>
-        <form action="bienvenido.php" method="POST">
-            <table width="600" align="center" cellpadding="15" cellspacing="0">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="shortcut icon" type="image/jpg" href="favicon.ico"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Motos Buffalo</title>
+</head>
+<body>
+<div class="container pt-4">
+    <form class="form-horizontal" action="bienvenido.php" method="POST">
+        <?php
+        if (isset($_REQUEST['error'])) {
+            if ($_REQUEST['error'] == 1) { ?>
+                <div class="alert alert-warning">Error al ingresar usuario o contraseña.</div>
                 <?php
-                if (isset($_REQUEST['error'])) {
-                    if ($_REQUEST['error'] == 1) {
-                        echo '<tr><td>Error al ingresar usuario o contraseña.</td></tr>';
-                    } else if ($_REQUEST['error'] == 2) {
-                        echo '<tr><td>Usted no tiene permisos para acceder a esta página.<br>
-                            Por favor registrese.
-                            </td></tr>';
-                    }
-                }
-                if (isset($_SESSION['user']) & isset($_REQUEST['param'])) {
-                    $_SESSION['user'] = null;
-                    echo '<tr><td>Gracias por utilizar el administrador.</td></tr>';
-                }
-                ?>
-                <tr>
-                    <td>
-                        <table width="400" border="0" align="center" cellpadding="0" cellspacing="0">
-                            <tr>
-                                <td>Usuario</td>
-                                <td><input type="text" id="user" name="user"/></td>
-                            </tr>
-                            <tr>
-                                <td>Contrase&ntilde;a</td>
-                                <td><input type="password" id="passwd" name="passwd"/></td>
-                            </tr>
+            } else if ($_REQUEST['error'] == 2) { ?>
+                <div class="alert alert-danger">Usted no tiene permisos para acceder a esta página.<br>
+                    Por favor registrese.
+                </div>
+                <?php
+            }
+        }
+        if (isset($_SESSION['user']) & isset($_REQUEST['param'])) {
+            $_SESSION['user'] = null;
+            echo '<div class="alert alert-info">Gracias por utilizar el administrador.</div>';
+        }
+        ?>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="user">Usuario</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="user" name="user" placeholder="Ingrese el usuario"/>
+            </div>
+        </div>
 
-                            <tr>
-                                <td style="text-align: center;" colspan="2">
-                                    <input type="submit"  id="btn_ingresar"  value="Aceptar"/>
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
-        </form>
-    </body>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="passwd">Contrase&ntilde;a</label>
+            <div class="col-sm-10">
+                <input type="password" class="form-control" id="passwd" name="passwd" placeholder="Ingrese el password"/>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <input type="submit" id="btn_ingresar" value="Aceptar" class="btn btn-primary"/>
+            </div>
+        </div>
+    </form>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+</body>
 </html>
